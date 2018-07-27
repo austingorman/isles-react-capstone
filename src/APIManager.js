bc1bf06fa4bbfae9a3bc88ca20c1b06e0a79b1b0
Austin Gorman <austin.gorman@gmail.com>
2018-07-26 19:02:21 -0500

    Some initial setup. It prints to the DOM

---
+++ src/APIManager.js
@@ -0,0 +1,43 @@
+const ApiManager = Object.create(
+  {},
+  {
+    getAll: {
+      value: collectionName => {
+        return fetch(`http://localhost:5002/${collectionName}`).then(e =>
+          e.json()
+        );
+      }
+    },
+    deleteItem: {
+      value: (collectionName, itemId) => {
+        return fetch(`http://localhost:5002/${collectionName}/${itemId}`, {
+          method: "DELETE"
+        });
+      }
+    },
+    postItem: {
+      value: (collectionName, theObject) => {
+        return fetch(`http://localhost:5002/${collectionName}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(theObject)
+        });
+      }
+    },
+    updateItem: {
+      value: (collectionName, itemId, dataObject) => {
+        return fetch(`http://localhost:5002/${collectionName}/${itemId}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(dataObject)
+        });
+      }
+    }
+  }
+);
+
+export default ApiManager;
